56400b316347a506e55419bff64fde588ae89343
Eric <ericld.eld@gmail.com>
2024-05-28 22:12:45 -0300

    desenvolvimento do main

--- frontend/src/components/pagehome/pagehome.tsx
+++ frontend/src/components/pagehome/pagehome.tsx
@@ -1,18 +1,14 @@
 import Header from "../header/header";
-// import Sections from "../section/sections";
+import TagMain from "../tagmain/tagMain";
+
 
 function PageHome(){
     return( 
         <>   
             <Header logoURL={'../image/logo/logo.png'}/>
         
-            {/* <TagMain/> */}
-                {/* <Sections 
-                titulo={'Lorem ipsum dolor'}
-                texto={
-                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam ducimus voluptatem consequatur incidunt dolor sit quod quibusdam magni dolores harum ullam libero similique aut. Natus porro magnam nostrum quia'
-                }
-                /> */}
+            <TagMain/>
+                
             {/* <Footer/> */}
         </>
     )
---
+++ frontend/src/components/tagmain/tagMain.tsx
@@ -0,0 +1,15 @@
+import './tagMain.css'
+import Sections from "../section/sections";
+function TagMain(){
+    return(
+        <main className="tagMain">
+            <Sections 
+                titulo={'Lorem ipsum dolor'}
+                texto={
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam ducimus voluptatem consequatur incidunt dolor sit quod quibusdam magni dolores harum ullam libero similique aut. Natus porro magnam nostrum quia'
+                }
+                />
+        </main>
+    )
+}
+export default TagMain;
