c9cd5e68415a7bb9b4829f34eb6702033493a7c6
Eric <ericld.eld@gmail.com>
2024-06-11 22:16:39 -0300

    atualizando o css depois de inserir a rota

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -2,20 +2,17 @@ import './header.css'
 import Config from './config/config'
 import Login from './login/login'
 import Bag from './bag/bag'
-import { Link } from 'react-router-dom'
 interface Props{
-    logoURL:string, 
-    linkHome:string,
-    linkMenuPage:string
+    logoURL: string,
 }
-function Header({logoURL, linkHome,linkMenuPage}:Props){
+function Header({logoURL}:Props){
     return(
         <header>
             <div className="img"><Config/></div>
-            <Link to={linkHome}><img src={logoURL} alt="logo" /></Link>
+            <div className="logo img h2"><img src={logoURL} alt="logo" /></div>
             <div className="img h3">
                 <Login/>
-                <Link to={linkMenuPage}><img src="../../../image/incons/menu.png" alt="menu"/></Link>
+                <img src="../../../image/incons/menu.png" alt="menu"/>
                 <Bag/>
             </div> 
         </header>
