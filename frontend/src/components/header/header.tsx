9d7f803637726921228b27d4f2831f2f67bb3dac
Eric <ericld.eld@gmail.com>
2024-06-11 22:26:50 -0300

    atualização dos links

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -2,6 +2,7 @@ import './header.css'
 import Config from './config/config'
 import Login from './login/login'
 import Bag from './bag/bag'
+import { Link } from 'react-router-dom'
 interface Props{
     logoURL: string,
 }
@@ -9,10 +10,10 @@ function Header({logoURL}:Props){
     return(
         <header>
             <div className="img"><Config/></div>
-            <div className="logo img h2"><img src={logoURL} alt="logo" /></div>
+            <Link to="/" className="logo img h2"><img src={logoURL} alt="logo" /></Link>
             <div className="img h3">
                 <Login/>
-                <img src="../../../image/incons/menu.png" alt="menu"/>
+                <Link to="menu"><img src="../../../image/incons/menu.png" alt="menu"/></Link>
                 <Bag/>
             </div> 
         </header>
