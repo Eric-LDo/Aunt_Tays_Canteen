e81fb6b6f0b0380db412f00cda7e68d32b88e88d
Eric <ericld.eld@gmail.com>
2024-06-22 18:28:06 -0300

    conclusão da inserção do backend

--- backend/cantinaTiaTay/src/main/java/com/example/cantinaTiaTay/repositories/ItensPedidosRepository.java
+++ backend/cantinaTiaTay/src/main/java/com/example/cantinaTiaTay/repositories/ItensPedidosRepository.java
@@ -5,6 +5,7 @@ import com.example.cantinaTiaTay.model.Pessoas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@SuppressWarnings("unused")
 @Repository
 public interface ItensPedidosRepository extends JpaRepository<ItensPedidos, Integer> {
 
